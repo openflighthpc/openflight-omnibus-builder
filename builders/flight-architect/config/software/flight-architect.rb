c6f85dd87d348f8ffa137df94fb52ad4b1255e09
stuts <stu.j.franks@gmail.com>
2019-07-10 11:37:00 +0100

    Bump architect version

--- builders/flight-architect/config/software/flight-architect.rb
+++ builders/flight-architect/config/software/flight-architect.rb
@@ -25,7 +25,7 @@
 # https://github.com/openflighthpc/openflight-omnibus-builder
 #===============================================================================
 name 'flight-architect'
-default_version '1.0.5'
+default_version '1.0.6'
 
 source git: 'https://github.com/openflighthpc/flight-architect'
 
