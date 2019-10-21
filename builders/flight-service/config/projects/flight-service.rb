7653276cf682477d0becfcb03daaa135785dd1aa
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2019-10-21 17:32:22 +0100

    Builder for `flight-service`

---
+++ builders/flight-service/config/projects/flight-service.rb
@@ -0,0 +1,60 @@
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-service'
+maintainer 'Alces Flight Ltd'
+homepage 'https://github.com/openflighthpc/flight-service'
+friendly_name 'Manage HPC environment services'
+
+install_dir '/opt/flight/opt/service'
+
+build_version '0.1.0'
+build_iteration 1
+
+dependency 'preparation'
+dependency 'flight-service'
+dependency 'version-manifest'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+
+description 'Manage HPC environment services'
+
+exclude '**/.git'
+exclude '**/.gitkeep'
+exclude '**/bundler/git'
+
+runtime_dependency 'flight-runway'
+runtime_dependency 'dialog'
+
+require 'find'
+Find.find('opt') do |o|
+  extra_package_file(o) if File.file?(o)
+end
+
+package :rpm do
+  vendor 'Alces Flight Ltd'
+end
