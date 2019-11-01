7ec46543a5be10bd510a194bffe0beb98d6909a5
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2019-11-01 11:37:35 +0000

    Complain if not root

--- scripts/enable-prod.sh
+++ scripts/enable-prod.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ "$UID" != "0" ]; then
+  echo "$0: must execute as root"
+  exit 1
+fi
 yum-config-manager --disable openflight-dev
 yum-config-manager --enable openflight
--- scripts/prepare-test.sh
+++ scripts/prepare-test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$UID" != "0" ]; then
+  echo "$0: must execute as root"
+  exit 1
+fi
 yum install -y epel-release
 yum install -y -e0 https://openflighthpc.s3-eu-west-1.amazonaws.com/repos/openflight/x86_64/openflighthpc-release-1-1.noarch.rpm
 yum makecache
