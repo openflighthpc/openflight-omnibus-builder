d66f75e6168ad18dd5a160deafedc971eb23ae7e
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2019-11-04 15:26:28 +0000

    Bump flight desktop to v1.1.3

--- builders/flight-desktop/config/projects/flight-desktop.rb
+++ builders/flight-desktop/config/projects/flight-desktop.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Desktop'
 
 install_dir '/opt/flight/opt/flight-desktop'
 
-build_version '1.1.2'
+build_version '1.1.3'
 build_iteration 1
 
 dependency 'preparation'
--- builders/flight-desktop/config/software/flight-desktop.rb
+++ builders/flight-desktop/config/software/flight-desktop.rb
@@ -25,7 +25,7 @@
 # https://github.com/openflighthpc/openflight-omnibus-builder
 #===============================================================================
 name 'flight-desktop'
-default_version '1.1.2'
+default_version '1.1.3'
 
 source git: 'https://github.com/openflighthpc/flight-desktop'
 
