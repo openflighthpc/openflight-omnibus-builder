b381c5de06b0144ded323a7c81a61f11aaea8480
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2019-12-11 17:28:20 +0000

    Bump flight-desktop version to 1.2.1

--- builders/flight-desktop/config/software/flight-desktop.rb
+++ builders/flight-desktop/config/software/flight-desktop.rb
@@ -25,7 +25,7 @@
 # https://github.com/openflighthpc/openflight-omnibus-builder
 #===============================================================================
 name 'flight-desktop'
-default_version '1.2.0'
+default_version '1.2.1'
 
 source git: 'https://github.com/openflighthpc/flight-desktop'
 
