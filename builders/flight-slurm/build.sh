c2ca32b68af2d62163671a0b08b1c275b78fbfc5
stuts <stu.j.franks@gmail.com>
2019-12-18 11:16:07 +0000

    Speed up repo clone and use relative rpmmacros

--- builders/flight-slurm/build.sh
+++ builders/flight-slurm/build.sh
@@ -11,17 +11,10 @@ sudo yum groupinstall -y "Development Tools"
 sudo yum install -y mysql-devel munge-devel munge-libs 
 
 # Create tarball
-git clone $REPO --branch $VERSION --single-branch $DIRECTORY
+git clone $REPO --branch $VERSION --depth 1 --single-branch $DIRECTORY
 tar --exclude='.git' -cvjSf $TARBALL $DIRECTORY
 
-# Update RPM build configuration
-if [ -e ~/.rpmmacros] ; then
-    echo "Backing up existing rpmmacros file"
-    mv -vf ~/.rpmmacros ~/.rpmmacros.omnibus-bup-$(date +%F_%H-%M)
-fi
-cp .rpmmacros ~/.rpmmacros
-
 # Build RPMs
-rpmbuild -ta $TARBALL
+rpmbuild --macros='/usr/lib/rpm/macros:/usr/lib/rpm/redhat/macros:/etc/rpm/macros:~/.rpmmacros:./.rpmmacros' -ta $TARBALL
 mkdir -p pkg
 mv $HOME/rpmbuild/RPMS/*/flight-slurm-*.rpm pkg
