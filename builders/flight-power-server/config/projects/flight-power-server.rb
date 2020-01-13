1915f8788e8a225590c37bdb86fed0cfd769f971
stuts <stu.j.franks@gmail.com>
2020-01-13 19:20:51 +0000

    More configuration setup and bump of build iteration

--- builders/flight-power-server/config/projects/flight-power-server.rb
+++ builders/flight-power-server/config/projects/flight-power-server.rb
@@ -32,7 +32,7 @@ friendly_name 'Flight Power Server'
 install_dir '/opt/flight/opt/power-server'
 
 build_version '0.3.2'
-build_iteration 1
+build_iteration 2
 
 dependency 'preparation'
 dependency 'flight-power-server'
