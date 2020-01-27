94541fa92a6bd714a3c49aa0abbf2a851d3bc00a
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2020-01-27 15:28:05 +0000

    Add more required build dependencies.

--- builders/flight-slurm/build.sh
+++ builders/flight-slurm/build.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")"
 
 # Install dependencies
 sudo yum groupinstall -y "Development Tools"
-sudo yum install -y mysql-devel munge-devel munge-libs 
+sudo yum install -y mysql-devel munge-devel munge-libs pam-devel readline-devel perl-devel lua-devel
 
 # Create tarball
 git clone $REPO --branch $VERSION --depth 1 --single-branch $DIRECTORY
