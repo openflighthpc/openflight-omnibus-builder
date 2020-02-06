e1b4e76b090f6a2ec3e7c18f6ffff904d120e90a
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2020-02-06 13:15:12 +0000

    Add `--with` options for `flight-slurm` build, bump to latest version of flight-slurm.

--- builders/flight-slurm/build.sh
+++ builders/flight-slurm/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-VERSION="slurm-17.11+bug_8073+rpmupdates"
+VERSION="slurm-19.05.5"
 REPO="https://github.com/openflighthpc/slurm"
 DIRECTORY="flight-${VERSION}"
 TARBALL="${DIRECTORY}.tar.bz2"
 
-cd "$(dirname "$0")" 
+cd "$(dirname "$0")"
 
 # Install dependencies
 sudo yum groupinstall -y "Development Tools"
-sudo yum install -y mysql-devel munge-devel munge-libs pam-devel readline-devel perl-devel lua-devel
+sudo yum install -y mysql-devel munge-devel munge-libs \
+     pam-devel readline-devel perl-devel lua-devel \
+     hwloc-devel numactl-devel pmix
 
 # Create tarball
 git clone $REPO --branch $VERSION --depth 1 --single-branch $DIRECTORY
@@ -20,6 +22,15 @@ rpmbuild --define '_prefix /opt/flight/opt/slurm' \
          --define '_defaultdocdir %{_prefix}/doc' \
          --define '_localstatedir %{_prefix}/var' \
          --define '_sharedstatedir %{_prefix}/var/lib' \
-         -ta $TARBALL
+         -ta $TARBALL \
+         --with hwloc \
+         --with mysql \
+         --with hdf5 \
+         --with lua \
+         --with numa \
+         --with x11 \
+         --without debug \
+         --with pmix
+
 mkdir -p pkg
 mv $HOME/rpmbuild/RPMS/*/flight-slurm-*.rpm pkg
