1b9b564cfb8d04e1960c5e6e49d9b894b46634ff
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2020-02-06 16:22:37 +0000

    Bump ruby version in builder VM for good measure

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -29,7 +29,7 @@ yum install -y -e0 git rpm-build cmake
 gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
-rvm install 2.6.3
+rvm install 2.7.0
 mkdir /opt/flight
 chown vagrant /opt/flight
 
