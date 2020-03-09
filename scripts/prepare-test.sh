ca06ce9ccb1c0f21501a4d193eb5f622f0032eea
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2020-03-09 18:28:18 +0000

    Make test prep script point to right repo

--- scripts/prepare-test.sh
+++ scripts/prepare-test.sh
@@ -4,5 +4,5 @@ if [ "$UID" != "0" ]; then
   exit 1
 fi
 yum install -y epel-release
-yum install -y -e0 https://openflighthpc.s3-eu-west-1.amazonaws.com/repos/openflight/x86_64/openflighthpc-release-1-1.noarch.rpm
+yum install -y -e0 https://repo.openflighthpc.org/openflight/centos/7/x86_64/openflighthpc-release-2-1.noarch.rpm
 yum makecache
