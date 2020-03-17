e8e402a72243ffcde5675e12197b73ddfb28b346
William McCumsite <william.mccumstie@gmail.com>
2020-03-17 15:40:47 +0000

    Create the flight-desktop-server builder

---
+++ builders/flight-desktop-server/config/projects/flight-desktop-server.rb
@@ -0,0 +1,69 @@
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'template'
+maintainer 'Alces Flight Ltd'
+homepage 'https://github.com/openflighthpc/template'
+friendly_name 'Flight Desktop'
+
+install_dir '/opt/flight/opt/template'
+
+build_version '1.1.0'
+build_iteration 1
+
+dependency 'preparation'
+dependency 'template'
+dependency 'version-manifest'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+
+description 'Manage interactive GUI desktop sessions'
+
+exclude '**/.git'
+exclude '**/.gitkeep'
+exclude '**/bundler/git'
+
+runtime_dependency 'flight-runway'
+
+%w(
+  tigervnc-server-minimal xorg-x11-xauth
+).each do |dep|
+  runtime_dependency dep
+end
+
+%w(
+  opt/flight/libexec/commands/desktop
+  opt/flight/etc/banner/tips.d/20-desktop.rc
+).each do |f|
+  extra_package_file f
+end
+
+package :rpm do
+  vendor 'Alces Flight Ltd'
+  # repurposed 'priority' field to set RPM recommends
+  #priority 'apg python-websockify xorg-x11-apps netpbm-progs'
+end
