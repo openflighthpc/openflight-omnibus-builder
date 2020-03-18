2ef03b33be8038314fc9d72770dc176b2cfbacc2
William McCumsite <william.mccumstie@gmail.com>
2020-03-18 16:48:00 +0000

    Use flight runway to build the package
    This ensures the paths are set correctly

--- builders/flight-desktop-server/config/software/enforce-flight-runway.rb
+++ builders/flight-desktop-server/config/software/enforce-flight-runway.rb
@@ -24,35 +24,13 @@
 # For more information on OpenFlight Omnibus Builder, please visit:
 # https://github.com/openflighthpc/openflight-omnibus-builder
 #===============================================================================
-name 'flight-desktop-server'
-default_version '0.1.2'
+name "enforce-flight-runway"
+default_version "0.0.0"
 
-source git: 'https://github.com/openflighthpc/flight-desktop-server'
-
-whitelist_file Regexp.new("vendor/ruby/.*\.so$")
-
-dependency 'enforce-flight-runway'
-
-license 'EPL-2.0'
-license_file 'LICENSE.txt'
-skip_transitive_dependency_licensing true
-
-build do
-  env = with_standard_compiler_flags(with_embedded_path)
-
-  # Moves the project into place
-  [
-    'Gemfile', 'Gemfile.lock', 'bin', 'etc', 'lib', 'libexec',
-    'LICENSE.txt', 'README.md'
-  ].each do |file|
-    copy file, File.expand_path("#{install_dir}/#{file}/..")
-  end
-
-  # Installs the gems to the shared `vendor/share`
-  flags = [
-    "--without development test",
-    '--path vendor'
-  ].join(' ')
-  command "cd #{install_dir} && /opt/flight/bin/bundle install #{flags}", env: env
+Dir.mktmpdir do |tmpdir|
+  source path: tmpdir
 end
 
+build do
+  raise "Flight Runway is not installed!" if ! File.exists?('/opt/flight/bin/flight')
+end
