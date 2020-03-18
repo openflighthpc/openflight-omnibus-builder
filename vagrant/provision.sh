4e1d95815d49c4f1b751670c175af42c7256c8f6
stuts <stu.j.franks@gmail.com>
2020-03-18 19:02:11 +0000

    Use Ruby 2.6 as CentOS8 with this omnibus and 2.7 breaks

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -30,7 +30,7 @@ if which yum &>/dev/null; then
   gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   curl -sSL https://get.rvm.io | bash -s stable
   source /etc/profile.d/rvm.sh
-  rvm install 2.7.0
+  rvm install 2.6
   gem install bundler:1.17.2
 
   # For libpcap compile (flight-metal)
