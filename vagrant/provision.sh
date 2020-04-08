4717c4a9cb04273b618628d289ed67c123e9e84e
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2020-04-08 18:21:30 +0100

    Retry RVM installation if it fails; bail out if it fails on retry. Install ruby 2.7.x, bundler v1.17.3 and bundler v2.1.4.
    Also bugfix hostname setting for builder machine.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -65,10 +65,25 @@ if [ "$1" != "test" ]; then
 
     yum install -y -e0 git rpm-build cmake
     gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-    curl -sSL https://get.rvm.io | bash -s stable
+    if ! curl -sSL https://get.rvm.io | bash -s stable; then
+      curl -sSL https://get.rvm.io | bash -s stable
+      if [ $? -gt 0 ]; then
+        cat <<EOF 1>&2
+
+!!!!!!!!!!!!!!!!!!!!!!!
+   RVM NOT INSTALLED
+!!!!!!!!!!!!!!!!!!!!!!!
+
+FSR, unable to install RVM. :-(
+
+EOF
+        exit 1
+      fi
+    fi
     source /etc/profile.d/rvm.sh
-    rvm install 2.6
-    gem install bundler:1.17.2
+    rvm install 2.7
+    gem install bundler:1.17.3
+    gem install bundler:2.1.4
     usermod -a -G rvm vagrant
 
     # For libpcap compile (flight-metal)
@@ -98,13 +113,14 @@ if [ "$1" != "test" ]; then
   elif which apt &>/dev/null; then
     apt update
     apt -y install ruby ruby-dev libffi-dev gcc make autoconf fakeroot
-    gem install bundler -v '< 2'
+    gem install bundler:1.17.3
+    gem install bundler:2.1.4
   fi
 
   mkdir /opt/flight
   chown vagrant /opt/flight
   hn=$(hostname -s)
-  if [ "$hostname" != "localhost" ]; then
+  if [ "$hn" != "localhost" ]; then
     hostname openflight-builder-${hn}
   else
     hostname openflight-builder
