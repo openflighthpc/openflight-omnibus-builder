96e4003717560645673500f82fefcb4a427bd3be
stuts <stu.j.franks@gmail.com>
2020-04-15 13:15:13 +0100

    Add slack check and update scripts

---
+++ scripts/slack-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+msg="
+:gift: _Package Published_ :gift:\n
+*Name:* \`$PACKAGE\`\n
+*Repo:* \`$REPO\`\n
+*Tree:* https://github.com/openflighthpc/openflight-omnibus-builder/commit/$(git rev-parse --short HEAD)"
+
+cat <<EOF | curl --data @- -X POST -H "Authorization: Bearer $SLACK_TOKEN" -H 'Content-Type: application/json' https://slack.com/api/chat.postMessage
+{
+  "text": "$msg",
+  "channel": "openflight",
+  "as_user": true
+}
+EOF
