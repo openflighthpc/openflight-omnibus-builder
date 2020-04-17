b2aef8d95663f7fbf0e16833e4cae4d8268117f7
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2020-04-17 16:35:57 +0100

    Add optional dependencies message for `flight-desktop` under CentOS 8 for `apg` and `python3-websockify`.

--- builders/flight-desktop/config/projects/flight-desktop.rb
+++ builders/flight-desktop/config/projects/flight-desktop.rb
@@ -32,7 +32,7 @@ friendly_name 'Flight Desktop'
 install_dir '/opt/flight/opt/desktop'
 
 build_version '1.3.0-rc6'
-build_iteration 1
+build_iteration 2
 
 dependency 'preparation'
 dependency 'flight-desktop'
