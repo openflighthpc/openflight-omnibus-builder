93956b70c14cbae2ef365335420052dc95d6ac2a
stuts <stu.j.franks@gmail.com>
2020-04-20 16:01:37 +0100

    Ubuntu promotion scripts

---
+++ scripts/promote-deb.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+# Promotes one or more debs between repos.
+set -e
+if [ ! -z "${DEBUG}" ]; then
+  set -x
+fi
+
+export SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <deb pattern> <distribution>"
+  exit 1
+fi
+
+if [ -z "$2" ] ; then
+    echo "Usage: $0 <deb pattern> <distribution>"
+    exit 1
+fi
+
+if ! aws s3 ls &>/dev/null; then
+  echo "$0: unable to access S3; check credentials?"
+  exit 1
+fi
+
+if ! gpg --list-secret-key 408ECC57 ; then
+    echo "Secret key not present!"
+    $SCRIPT_DIR/install-gpg.sh
+fi
+
+# Check Slack notifications configured
+$SCRIPT_DIR/slack-check.sh
+
+export DEB_PATTERN="$1"
+DIST="$2"
+$SCRIPT_DIR/promote-debs.sh -s "repo.openflighthpc.org/openflight-dev/ubuntu" \
+                            -t "repo.openflighthpc.org/openflight/ubuntu" \
+                            -r "$DEB_PATTERN" \
+                            -d "$DIST"
