cfa8286cb9fd4f91a0fec6430426928b81fa2f75
Ben Armston <ben.armston@alces-software.com>
2020-07-17 16:12:41 +0100

    Bump build iteration

--- builders/flight-www/config/projects/flight-www.rb
+++ builders/flight-www/config/projects/flight-www.rb
@@ -35,7 +35,7 @@ VERSION = '1.1.0-rc4'
 override 'flight-www', version: VERSION
 
 build_version VERSION
-build_iteration '1'
+build_iteration '2'
 
 dependency 'preparation'
 dependency 'flight-www'
