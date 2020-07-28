85c3f650128e99c69f2befbb128d0eae65f6594a
Ben Armston <ben.armston@alces-software.com>
2020-07-28 15:06:14 +0100

    Add flight estate builders

---
+++ builders/flight-estate/config/projects/flight-estate.rb
@@ -0,0 +1,83 @@
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-estate'
+maintainer 'Alces Flight Ltd'
+homepage 'https://github.com/openflighthpc/flight-estate'
+friendly_name 'Flight Estate'
+
+install_dir '/opt/flight/opt/estate'
+
+VERSION = '0.6.0'
+override 'flight-estate', version: VERSION
+
+build_version VERSION
+build_iteration 1
+
+dependency 'preparation'
+dependency 'version-manifest'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+
+description 'Modify your compute estate'
+
+exclude '**/.git'
+exclude '**/.gitkeep'
+exclude '**/bundler/git'
+
+runtime_dependency 'flight-action'
+
+%w(
+  opt/flight/etc/banner/tips.d/40-estate.rc
+  opt/flight/libexec/commands/estate
+).each do |f|
+  extra_package_file f
+end
+
+# Update the version numbering in files
+File.expand_path('../../opt/flight/libexec/commands/estate', __dir__).tap do |path|
+  content = File.read path
+  content.sub!(/: VERSION:.*/, ": VERSION: #{VERSION}")
+  File.write path, content
+end
+
+if ohai['platform_family'] == 'rhel'
+  rhel_rel = ohai['platform_version'].split('.').first.to_i
+  if rhel_rel == 8
+    package :rpm do
+      vendor 'Alces Flight Ltd'
+    end
+  else
+    package :rpm do
+      vendor 'Alces Flight Ltd'
+    end
+  end
+end
+
+package :deb do
+  vendor 'Alces Flight Ltd'
+end
