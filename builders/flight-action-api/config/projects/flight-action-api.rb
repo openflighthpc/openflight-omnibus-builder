649cbf25740ae63776331273c92e01dbe297dc82
William McCumsite <william.mccumstie@gmail.com>
2020-08-07 13:50:50 +0100

    Demote build iteration to 1.0.0-2
    The 3 was added as the result of a rebase (I think?)

--- builders/flight-action-api/config/projects/flight-action-api.rb
+++ builders/flight-action-api/config/projects/flight-action-api.rb
@@ -35,7 +35,7 @@ VERSION = '1.1.0'
 override 'flight-action-api', version: VERSION
 
 build_version VERSION
-build_iteration 3
+build_iteration 2
 
 dependency 'preparation'
 dependency 'flight-action-api'
