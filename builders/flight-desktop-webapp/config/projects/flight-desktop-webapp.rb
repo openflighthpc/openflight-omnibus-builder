507f8883b3a50914a5f5349d2e873d2a30e98e7e
William McCumsite <william.mccumstie@gmail.com>
2020-08-10 12:14:12 +0100

    Remove https redirection from flight-desktop-webapp
    This is now handled by `flight-www`

--- builders/flight-desktop-webapp/config/projects/flight-desktop-webapp.rb
+++ builders/flight-desktop-webapp/config/projects/flight-desktop-webapp.rb
@@ -35,7 +35,7 @@ VERSION = '1.2.0'
 override 'flight-desktop-webapp', version: VERSION
 
 build_version VERSION
-build_iteration 2
+build_iteration 3
 
 dependency 'preparation'
 dependency 'flight-desktop-webapp'
