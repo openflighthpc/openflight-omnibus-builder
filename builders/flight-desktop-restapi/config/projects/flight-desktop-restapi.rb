643156a9a233fd1c6343b90f91f82881867b831a
William McCumsite <william.mccumstie@gmail.com>
2020-08-11 13:50:54 +0100

    Bump the build iteration for flight-desktop-restapi

--- builders/flight-desktop-restapi/config/projects/flight-desktop-restapi.rb
+++ builders/flight-desktop-restapi/config/projects/flight-desktop-restapi.rb
@@ -35,7 +35,7 @@ VERSION = '1.0.1'
 override 'flight-desktop-restapi', version: VERSION
 
 build_version VERSION
-build_iteration 6
+build_iteration 7
 
 dependency 'preparation'
 dependency 'flight-desktop-restapi'
