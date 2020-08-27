84cde6884dae298164a722511235d0d7754a5a16
William McCumsite <william.mccumstie@gmail.com>
2020-08-27 16:24:23 +0100

    Bump flight-action-api-estate to version 1.2.0-rc1

--- builders/flight-action-api-estate/config/projects/flight-action-api-estate.rb
+++ builders/flight-action-api-estate/config/projects/flight-action-api-estate.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Action API estate management actions'
 
 install_dir '/opt/flight/opt/action-api'
 
-VERSION = '1.1.0'
+VERSION = '1.2.0-rc1'
 override 'flight-action-api-estate', version: VERSION
 
 build_version VERSION
