f198e4c2db765de96458b08168a98282f3a08070
William McCumsite <william.mccumstie@gmail.com>
2020-08-27 12:38:05 +0100

    Bump flight-action to version 1.2.0-rc1

--- builders/flight-action/config/projects/flight-action.rb
+++ builders/flight-action/config/projects/flight-action.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Action'
 
 install_dir '/opt/flight/opt/action'
 
-VERSION = '1.1.0'
+VERSION = '1.2.0-rc1'
 override 'flight-action', version: VERSION
 
 build_version VERSION
