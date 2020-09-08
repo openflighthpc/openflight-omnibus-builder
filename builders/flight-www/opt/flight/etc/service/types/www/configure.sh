6f81f6eb59d711801ca1b74c6d5583c983cce44f
William McCumsite <william.mccumstie@gmail.com>
2020-09-08 12:09:12 +0100

    Remove the configuration of https symlink
    The main config is symlinked to the disabled config and does not need to
    be updated independently

--- builders/flight-www/opt/flight/etc/service/types/www/configure.sh
+++ builders/flight-www/opt/flight/etc/service/types/www/configure.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+#==============================================================================
+# Copyright (C) 2020-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+
 echo "Configuring"
 mkdir -p "${flight_SERVICE_etc}"
 >"${flight_SERVICE_etc}/example.rc"
@@ -14,10 +41,6 @@ for a in "$@"; do
     https_port)
       if [ -f "${flight_ROOT}"/etc/www/http.d/https.conf.disabled ] ; then
           sed -i "${flight_ROOT}"/etc/www/http.d/https.conf.disabled \
-              -e "s/^\s*listen\s.*;/listen ${v} ssl default;/g"
-      fi
-      if [ -f "${flight_ROOT}"/etc/www/http.d/https.conf ] ; then
-          sed -i "${flight_ROOT}"/etc/www/http.d/https.conf \
               -e "s/^\s*listen\s.*;/listen ${v} ssl default;/g"
       fi
     ;;
