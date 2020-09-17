bfe56acabd2b137350349a341862df62eebc98ce
William McCumsite <william.mccumstie@gmail.com>
2020-09-17 16:24:27 +0100

    Fixed bug where the non-managed PIDFILE is incorrect
    The input is the path to the managed pidfile not the PID itself

--- builders/flight-scheduler-daemon/opt/flight/etc/service/types/scheduler-daemon/restart.sh
+++ builders/flight-scheduler-daemon/opt/flight/etc/service/types/scheduler-daemon/restart.sh
@@ -40,7 +40,7 @@ function cleanup() {
 trap cleanup EXIT
 
 # Setup the non-managed PIDFILE
-echo "$1" > flight-scheduler-daemon.pid
+cp "$1" flight-scheduler-daemon.pid
 
 # Creates the log directory
 LOG_DIR="$flight_ROOT"/var/log/scheduler-daemon
