9152136143b43bce08e24042d8e97e5a44bdbc80
William McCumsite <william.mccumstie@gmail.com>
2020-10-08 13:43:10 +0100

    Include the etc directory in flight-scheduler-daemon

--- builders/flight-scheduler-daemon/config/software/flight-scheduler-daemon.rb
+++ builders/flight-scheduler-daemon/config/software/flight-scheduler-daemon.rb
@@ -42,7 +42,7 @@ build do
 
   # Moves the project into place
   [
-    'Gemfile', 'Gemfile.lock', 'bin', 'config', 'lib', 'LICENSE.txt', 'README.md'
+    'Gemfile', 'Gemfile.lock', 'bin', 'config', 'etc', 'lib', 'LICENSE.txt', 'README.md'
   ].each do |file|
     copy file, File.expand_path("#{install_dir}/#{file}/..")
   end
