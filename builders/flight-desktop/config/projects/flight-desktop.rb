98e07ffbefb432608fe9e241e09f306f1a33b8a8
William McCumsite <william.mccumstie@gmail.com>
2020-10-13 18:23:16 +0100

    Add a howto guide for flight-desktop

--- builders/flight-desktop/config/projects/flight-desktop.rb
+++ builders/flight-desktop/config/projects/flight-desktop.rb
@@ -55,6 +55,18 @@ runtime_dependency 'flight-runway'
 runtime_dependency 'flight-ruby-system-2.0'
 runtime_dependency 'flight-desktop-types'
 
+Dir.glob('opt/**/*')
+   .select { |p| File.file? p }
+   .each { |p| extra_package_file p }
+
+# Moves the correct howto version into place
+howto_src = File.expand_path("../../contrib/howto/#{VERSION.sub(/\.\d+(\.[abcr].*)?\Z/, '')}", __dir__)
+howto_dst = File.expand_path("../../opt/flight/usr/share/howto/flight-desktop.md", __dir__)
+raise "Could not locate: #{howto_src}" unless File.exists? howto_src
+FileUtils.mkdir_p File.dirname(howto_dst)
+FileUtils.rm_f howto_dst
+FileUtils.cp howto_src, howto_dst
+
 if ohai['platform_family'] == 'rhel'
   runtime_dependency 'tigervnc-server-minimal'
   runtime_dependency 'xorg-x11-xauth'
@@ -63,13 +75,6 @@ elsif ohai['platform_family'] == 'debian'
   runtime_dependency 'tigervnc-standalone-server'
   runtime_dependency 'xauth'
   runtime_dependency 'perl'
-end
-
-%w(
-  opt/flight/libexec/commands/desktop
-  opt/flight/etc/banner/tips.d/20-desktop.rc
-).each do |f|
-  extra_package_file f
 end
 
 if ohai['platform_family'] == 'rhel'
