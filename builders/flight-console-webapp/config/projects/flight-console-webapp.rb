82db1723092f9aa616f9e48fa5ad1fa62fc5e616
William McCumsite <william.mccumstie@gmail.com>
2020-10-19 11:34:36 +0100

    Reload nginx after installing flight-console-webapp

--- builders/flight-console-webapp/config/projects/flight-console-webapp.rb
+++ builders/flight-console-webapp/config/projects/flight-console-webapp.rb
@@ -35,7 +35,7 @@ VERSION = '1.0.0'
 override 'flight-console-webapp', version: VERSION
 
 build_version VERSION
-build_iteration 1
+build_iteration 2
 
 dependency 'preparation'
 dependency 'flight-console-webapp'
