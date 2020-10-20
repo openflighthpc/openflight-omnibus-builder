d3d7505f322ddf60df105bce8f6cc71dd97ac186
William McCumsite <william.mccumstie@gmail.com>
2020-10-20 13:19:07 +0100

    Add a debian builder for flight-web-suite

--- builders/flight-web-suite/build.sh
+++ builders/flight-web-suite/build.sh
@@ -1,5 +1,70 @@
 #!/bin/bash
-cd "$(dirname "$0")" 
-rpmbuild -bb flight-web-suite.spec
+#==============================================================================
+# Copyright (C) 2020-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+
+cd "$(dirname "$0")"
+d="$(pwd)"
 mkdir -p pkg
-mv $HOME/rpmbuild/RPMS/noarch/flight-web-suite-*.noarch.rpm pkg
+
+NAME=flight-web-suite
+VERSION=2020.3
+REL=2
+
+if [ -f /etc/redhat-release ]; then
+  echo "Building RPM package..."
+  cd rpm
+  rpmbuild -bb ${NAME}.spec \
+           --define "_flight_pkg_version $VERSION" \
+           --define "_flight_pkg_rel $REL"
+  cd ..
+  mv $HOME/rpmbuild/RPMS/noarch/${NAME}-*.noarch.rpm pkg
+elif [ -f /etc/lsb-release ]; then
+  echo "Building DEB package..."
+
+  mkdir -p $HOME/${NAME}
+
+  pushd $HOME/${NAME}
+  rm -rf ${VERSION}.tar.gz ${NAME}-${VERSION}
+  mkdir -p ${NAME}_${VERSION}-${REL}/DEBIAN
+  sed -e "s/%VERSION%/$VERSION/g" \
+      -e "s/%REL%/$REL/g" \
+      $d/deb/control.tpl > \
+      $HOME/${NAME}/${NAME}_${VERSION}-${REL}/DEBIAN/control
+
+  pushd ${NAME}_${VERSION}-$REL
+  wget https://raw.githubusercontent.com/openflighthpc/openflight-omnibus-builder/master/LICENSE.txt
+  mkdir -p usr/share/doc/${NAME}
+  mv LICENSE.txt usr/share/doc/${NAME}
+  popd
+
+  fakeroot dpkg-deb --build ${NAME}_${VERSION}-${REL}
+  popd
+
+  mv $HOME/${NAME}/*.deb pkg
+else
+  echo "Couldn't determine type of package to build."
+fi
---
+++ builders/flight-web-suite/deb/control.tpl
@@ -0,0 +1,11 @@
+Package:      flight-web-suite
+Version:      %VERSION%-%REL%
+Section:      misc
+Priority:     optional
+Architecture: all
+Maintainer:   OpenFlightHPC Project <flight@openflighthpc.org>
+Description:  The Flight User Suite collection of HPC environment tools
+Depends:      flight-console-api (>= 1.0.0), flight-console-api (<< 1.1.0),
+  flight-console-webapp (>= 1.0.0), flight-console-webapp (<< 1.1.0),
+  flight-desktop-restapi (>= 1.0.2), flight-desktop-restapi (<< 1.1.0),
+  flight-desktop-webapp (>= 1.2.0), flight-desktop-webapp (<< 1.3.0)
