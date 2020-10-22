5759d48ac1433e263775d0845bb8496c8cf8945e
William McCumsite <william.mccumstie@gmail.com>
2020-10-22 18:16:00 +0100

    Bump flight-estate build iteration for howtos

--- builders/flight-estate/config/projects/flight-estate.rb
+++ builders/flight-estate/config/projects/flight-estate.rb
@@ -35,7 +35,7 @@ VERSION = '1.1.0'
 override 'flight-estate', version: VERSION
 
 build_version VERSION
-build_iteration 1
+build_iteration 2
 
 dependency 'preparation'
 dependency 'version-manifest'
