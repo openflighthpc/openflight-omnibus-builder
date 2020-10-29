1400edc23873a563cd662f4086d1ca085a315acf
William McCumsite <william.mccumstie@gmail.com>
2020-10-29 15:31:41 +0000

    Bump flight-action-api to version 1.3.0~rc1

--- builders/flight-action-api/config/projects/flight-action-api.rb
+++ builders/flight-action-api/config/projects/flight-action-api.rb
@@ -31,11 +31,11 @@ friendly_name 'Flight Action API'
 
 install_dir '/opt/flight/opt/action-api'
 
-VERSION = '1.2.0'
+VERSION = '1.3.0-rc1'
 override 'flight-action-api', version: VERSION
 
 build_version VERSION
-build_iteration 2
+build_iteration 1
 
 dependency 'preparation'
 dependency 'flight-action-api'
