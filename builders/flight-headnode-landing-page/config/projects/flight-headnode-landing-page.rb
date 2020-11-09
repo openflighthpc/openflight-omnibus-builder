4300a597627be581e428a9d7fb617aa2d05c16f3
Ben Armston <ben.armston@alces-software.com>
2020-11-09 13:53:16 +0000

    Builder for landing page content

---
+++ builders/flight-headnode-landing-page/config/projects/flight-headnode-landing-page.rb
@@ -0,0 +1,74 @@
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-headnode-landing-page'
+maintainer 'Alces Flight Ltd'
+homepage 'https://github.com/openflighthpc/openflight-omnibus-builders/builders/flight-headnode-landing-page'
+friendly_name 'Flight web server service'
+
+install_dir '/opt/flight/opt/www/landing-page'
+
+VERSION = '1.1.0-rc1'
+override 'flight-headnode-landing-page', version: VERSION
+
+build_version VERSION
+build_iteration '1'
+
+dependency 'preparation'
+dependency 'flight-headnode-landing-page'
+dependency 'version-manifest'
+
+if ohai['platform_family'] == 'rhel'
+  conflict 'flight-www < 1.3.0'
+elsif ohai['platform_family'] == 'debian'
+  conflict 'flight-www (< 1.3.0)'
+else
+  raise "Unrecognised platform: #{ohai['platform_family']}"
+end
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+
+description 'A headnode landing page for use in Flight HPC environments'
+
+exclude '**/.git'
+exclude '**/.gitkeep'
+exclude '**/bundler/git'
+
+package :rpm do
+  vendor 'Alces Flight Ltd'
+  # repurposed 'priority' field to set RPM recommends/provides
+  # provides are prefixed with `:`
+  priority ":flight-landing-page-content"
+end
+
+package :deb do
+  vendor 'Alces Flight Ltd'
+  # repurposed 'section' field to set DEB recommends/provides
+  # entire section is prefixed with `:` to trigger handling
+  # provides are further prefixed with `:`
+  section "::flight-landing-page-content"
+end
---
+++ builders/flight-headnode-landing-page/config/software/flight-headnode-landing-page.rb
@@ -0,0 +1,40 @@
+#==============================================================================
+# Copyright (C) 2020-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-headnode-landing-page'
+default_version '0.0.0'
+
+source git: 'https://github.com/openflighthpc/flight-landing-page'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+skip_transitive_dependency_licensing true
+
+build do
+  type = 'headnode'
+  # Moves the content for the landing page into place.
+  copy "landing-page/types/#{type}", "#{install_dir}/default"
+end
