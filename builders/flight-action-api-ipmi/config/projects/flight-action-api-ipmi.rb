a7b76075cb98010bb0f81c111215b40fbb4910e0
William McCumsite <william.mccumstie@gmail.com>
2020-11-12 17:40:10 +0000

    Initial commit creating flight-action-api-ipmi

---
+++ builders/flight-action-api-ipmi/config/projects/flight-action-api-ipmi.rb
@@ -0,0 +1,63 @@
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-action-api-ipmi'
+maintainer 'Alces Flight Ltd'
+homepage "https://github.com/openflighthpc/flight-action-api"
+friendly_name 'Flight Action API ipmi actions'
+
+install_dir '/opt/flight/opt/action-api'
+
+VERSION = '1.4.0-rc3'
+override 'flight-action-api-ipmi', version: VERSION
+
+build_version VERSION
+build_iteration 1
+
+dependency 'preparation'
+dependency 'flight-action-api-ipmi'
+
+text_manifest_path File.join(install_dir, "version-manifest.ipmi-actions.txt")
+json_manifest_path File.join(install_dir, "version-manifest.ipmi-actions.json")
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+license_file_path 'LICENSE.ipmi-actions'
+
+description 'Power actions for Flight Action API'
+
+exclude '**/.git'
+exclude '**/.gitkeep'
+
+runtime_dependency 'flight-action-api'
+
+package :rpm do
+  vendor 'Alces Flight Ltd'
+end
+
+package :deb do
+  vendor 'Alces Flight Ltd'
+end
---
+++ builders/flight-action-api-ipmi/config/software/flight-action-api-ipmi.rb
@@ -0,0 +1,47 @@
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-action-api-ipmi'
+default_version '0.0.0'
+
+source git: 'https://github.com/openflighthpc/flight-action-api'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+skip_transitive_dependency_licensing true
+
+build do
+  block do
+    # Moves the project into place
+    [
+      'ipmi-sel'
+    ].each do |action|
+      libexec_dir = File.expand_path(File.join(install_dir, 'libexec'))
+      FileUtils.mkdir_p File.join(libexec_dir, action)
+      copy File.join('libexec', action), libexec_dir
+    end
+  end
+end
