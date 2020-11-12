7ec1334b9b98dd3d3055b7bf7d96de39c40fffb2
William McCumsite <william.mccumstie@gmail.com>
2020-11-12 17:48:00 +0000

    Initial commit creating flight-ipmi

---
+++ builders/flight-ipmi/config/projects/flight-ipmi.rb
@@ -0,0 +1,73 @@
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-ipmi'
+maintainer 'Alces Flight Ltd'
+homepage 'https://github.com/openflighthpc/flight-ipmi'
+friendly_name 'Flight Power'
+
+install_dir '/opt/flight/opt/ipmi'
+
+VERSION = '1.1.0'
+override 'flight-ipmi', version: VERSION
+
+build_version VERSION
+build_iteration 2
+
+dependency 'preparation'
+dependency 'version-manifest'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+
+description 'Execute ipmi actions on flight clusters'
+
+exclude '**/.git'
+exclude '**/.gitkeep'
+exclude '**/bundler/git'
+
+runtime_dependency 'flight-action'
+
+%w(
+  opt/flight/libexec/commands/ipmi
+).each do |f|
+  extra_package_file f
+end
+
+# Update the version numbering in files
+File.expand_path('../../opt/flight/libexec/commands/ipmi', __dir__).tap do |path|
+  content = File.read path
+  content.sub!(/: VERSION:.*/, ": VERSION: #{VERSION}")
+  File.write path, content
+end
+
+package :rpm do
+  vendor 'Alces Flight Ltd'
+end
+
+package :deb do
+  vendor 'Alces Flight Ltd'
+end
