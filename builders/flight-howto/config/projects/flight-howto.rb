a13c5b2b2890fc58ad1b8a198d493a411cfbe486
Ben Armston <ben.armston@alces-software.com>
2020-11-24 17:14:31 +0000

    Bump flight-howto version

--- builders/flight-howto/config/projects/flight-howto.rb
+++ builders/flight-howto/config/projects/flight-howto.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Inventory'
 
 install_dir '/opt/flight/opt/howto'
 
-VERSION = '1.1.0-rc3'
+VERSION = '1.1.0-rc5'
 override 'flight-howto', version: VERSION
 
 build_version VERSION
