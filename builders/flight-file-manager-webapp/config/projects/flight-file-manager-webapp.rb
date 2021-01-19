a7e4d36c2470c7f2d916ab83323a850ad881ecd2
William McCumsite <william.mccumstie@gmail.com>
2021-01-19 15:56:06 +0000

    Initial commit createing flight-file-manager-webapp

---
+++ builders/flight-file-manager-webapp/config/projects/flight-file-manager-webapp.rb
@@ -0,0 +1,70 @@
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-file-manager-webapp'
+maintainer 'Alces Flight Ltd'
+homepage 'https://github.com/openflighthpc/flight-file-manager-webapp'
+friendly_name 'Flight File Manager Webapp'
+
+install_dir '/opt/flight/opt/file-manager-webapp'
+
+VERSION = '0.1.0-a1'
+override 'flight-file-manager-webapp', version: VERSION
+
+build_version VERSION
+build_iteration 1
+
+dependency 'preparation'
+dependency 'flight-file-manager-webapp'
+dependency 'version-manifest'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+
+description 'Manage file manager sessions'
+
+exclude '**/.git'
+exclude '**/.gitkeep'
+exclude '**/bundler/git'
+exclude 'node_modules'
+
+runtime_dependency 'flight-service'
+runtime_dependency 'flight-service-system-1.0'
+runtime_dependency 'flight-www'
+runtime_dependency 'flight-www-system-1.0'
+
+require 'find'
+Find.find('opt') do |o|
+  extra_package_file(o) if File.file?(o)
+end
+
+package :rpm do
+  vendor 'Alces Flight Ltd'
+end
+
+package :deb do
+  vendor 'Alces Flight Ltd'
+end
---
+++ builders/flight-file-manager-webapp/opt/flight/etc/service/types/file-manager-webapp/configure.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+echo "Configuring"
+
+set_string() {
+    local key
+    local value
+    key="$1"
+    value=$( echo "${2}" | sed -s 's/"/\\"/g' )
+
+    # NOTE: This script is practically identical to `set_nil`.  If changing
+    # one, change the other.
+  "${flight_ROOT}/bin/ruby" <<EOF
+require 'json'
+if File.exist?('/opt/flight/opt/file-manager-webapp/build/config.json')
+  json = File.read('/opt/flight/opt/file-manager-webapp/build/config.json')
+  config = JSON.parse(json)
+else
+  config = {
+    "websocketPathPrefix" => "/ws",
+    "websocketPathIp" => "127.0.0.1",
+  }
+end
+config["${key}"] = "${value}"
+new_json = JSON.pretty_generate(config)
+File.write('/opt/flight/opt/file-manager-webapp/build/config.json', new_json)
+EOF
+}
+
+set_nil() {
+    local key
+    key="$1"
+
+
+    # NOTE: This script is practically identical to `set_string`.  If changing
+    # one, change the other.
+  "${flight_ROOT}/bin/ruby" <<EOF
+require 'json'
+if File.exist?('/opt/flight/opt/file-manager-webapp/build/config.json')
+  json = File.read('/opt/flight/opt/file-manager-webapp/build/config.json')
+  config = JSON.parse(json)
+else
+  config = {
+    "websocketPathPrefix" => "/ws",
+    "websocketPathIp" => "127.0.0.1",
+  }
+end
+config["${key}"] = nil
+new_json = JSON.pretty_generate(config)
+File.write('/opt/flight/opt/file-manager-webapp/build/config.json', new_json)
+EOF
+}
+
+for a in "$@"; do
+  IFS="=" read k v <<< "${a}"
+  case $k in
+    clusterName)
+      set_string clusterName "${v}"
+      ;;
+
+    clusterDescription)
+      set_string clusterDescription "${v}"
+      ;;
+
+    clusterLogo)
+      if [[ -z "${v// }" ]]; then
+          set_nil clusterLogo
+      else
+          set_string clusterLogo "${v}"
+      fi
+      ;;
+
+    hostname)
+      url="https://${v}/files/api"
+      set_string apiRootUrl "${url}"
+      ;;
+    *)
+      echo "Unrecognised key: $k"
+    ;;
+  esac
+done
