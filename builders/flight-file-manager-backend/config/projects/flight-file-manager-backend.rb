1210bab419bbb2b7bdab9e929e296b7f9e5f74c8
Ben Armston <ben.armston@alces-software.com>
2021-01-27 12:14:38 +0000

    Aborted attempt at a separate RPM for file manager backend
    There was an issue with building the RPM for file-manager-api after the
    backend was added as a software component.  This commit adds a new
    builder for the file manager backend as part of debugging that problem.
    
    Ultimately it was not needed and will be reverted in the next commit.
    The issue was due to a dependency containing `.git` submodules in its
    packaged files.

---
+++ builders/flight-file-manager-backend/config/projects/flight-file-manager-backend.rb
@@ -0,0 +1,69 @@
+#==============================================================================
+# Copyright (C) 2020-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-file-manager-backend'
+maintainer 'Alces Flight Ltd'
+homepage "https://github.com/openflighthpc/flight-file-manager"
+friendly_name 'Flight File Manager Backend'
+
+install_dir '/opt/flight/opt/file-manager-backend'
+
+VERSION = '0.1.0-a3'
+override 'flight-file-manager-backend', version: VERSION
+
+build_version VERSION
+build_iteration 1
+
+dependency 'preparation'
+dependency 'flight-file-manager-backend'
+dependency 'version-manifest'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+
+description 'Backend providing Flight file manager sessions'
+
+exclude '**/.git'
+exclude '**/.gitkeep'
+
+runtime_dependency 'flight-service-system-1.0'
+runtime_dependency 'flight-nodejs'
+runtime_dependency 'flight-js-system-1.0'
+runtime_dependency 'flight-www'
+runtime_dependency 'flight-www-system-1.0'
+
+require 'find'
+Find.find('opt') do |o|
+  extra_package_file(o) if File.file?(o)
+end
+
+package :rpm do
+  vendor 'Alces Flight Ltd'
+end
+
+package :deb do
+  vendor 'Alces Flight Ltd'
+end
---
+++ builders/flight-file-manager-backend/config/software/flight-file-manager-backend.rb
@@ -0,0 +1,66 @@
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-file-manager-backend'
+default_version '0.0.0'
+
+source git: 'https://github.com/openflighthpc/flight-file-manager'
+
+dependency 'enforce-flight-nodejs'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+skip_transitive_dependency_licensing true
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  # Moves the shared project into place
+  ['LICENSE.txt'].each do |file|
+    copy file, File.expand_path("#{install_dir}/#{file}/..")
+  end
+
+  # Moves the project into place
+  [
+    'package.json', 'yarn.lock', 'src', 'etc',
+    'LICENSE.txt',
+    #'README.md',
+  ].each do |file|
+    copy File.join('backend', file), File.expand_path("#{install_dir}/#{file}/..")
+  end
+
+  command "cd #{install_dir} && /opt/flight/bin/yarn install", env: env
+
+  block do
+    # Remove some git submodule files in some dependencies.  If these are left
+    # in, the build caching mechanism used by omnibus breaks which in turn
+    # breaks the build of this software.
+    %w(spec stone).each do |dir|
+      path = File.join(install_dir, "node_modules/dropbox/generator", dir, ".git")
+      FileUtils.rm_rf(path)
+    end
+  end
+end
