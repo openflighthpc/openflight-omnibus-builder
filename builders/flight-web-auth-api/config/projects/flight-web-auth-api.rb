5106afd390e0cc944171a7bb9509590082d6a20e
William McCumsite <william.mccumstie@gmail.com>
2021-02-15 10:58:42 +0000

    Initial commit creating flight-web-auth-api

---
+++ builders/flight-web-auth-api/config/projects/flight-web-auth-api.rb
@@ -0,0 +1,79 @@
+#==============================================================================
+# Copyright (C) 2021-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-web-auth-api'
+maintainer 'Alces Flight Ltd'
+homepage "https://github.com/openflighthpc/flight-web-auth-api"
+friendly_name 'Flight Job Script API'
+
+install_dir '/opt/flight/opt/web-auth-api'
+
+VERSION = '0.2.1'
+override 'flight-web-auth-api', version: VERSION
+
+build_version VERSION
+build_iteration 1
+
+dependency 'preparation'
+dependency 'flight-web-auth-api'
+dependency 'version-manifest'
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+
+description 'API server for authenticating cluster users'
+
+exclude '**/.git'
+exclude '**/.gitkeep'
+exclude '**/bundler/git'
+
+# RPam dependencies
+runtime_dependency 'pam'
+runtime_dependency 'audit-libs'
+runtime_dependency 'libcap-ng'
+
+# Flight Dependencies
+runtime_dependency 'flight-runway'
+runtime_dependency 'flight-ruby-system-2.0'
+runtime_dependency 'flight-www'
+runtime_dependency 'flight-www-system-1.0'
+runtime_dependency 'flight-service'
+runtime_dependency 'flight-service-system-1.0'
+
+config_file File.join(install_dir, 'etc/flight-web-auth.yaml')
+
+require 'find'
+Find.find('opt') do |o|
+  extra_package_file(o) if File.file?(o)
+end
+
+package :rpm do
+  vendor 'Alces Flight Ltd'
+end
+
+package :deb do
+  vendor 'Alces Flight Ltd'
+end
---
+++ builders/flight-web-auth-api/config/software/flight-web-auth-api.rb
@@ -0,0 +1,72 @@
+#==============================================================================
+# Copyright (C) 2021-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+name 'flight-web-auth-api'
+default_version '0.0.0'
+
+source git: 'https://github.com/openflighthpc/flight-web-auth-api'
+
+dependency 'enforce-flight-runway'
+
+whitelist_file Regexp.new("vendor/ruby/.*.so")
+
+license 'EPL-2.0'
+license_file 'LICENSE.txt'
+skip_transitive_dependency_licensing true
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  block do
+    FileUtils.mkdir_p File.join(install_dir,  'etc')
+  end
+
+  # Moves the api project into place
+  [
+    'Gemfile', 'Gemfile.lock', 'README.md', 'LICENSE.txt', 'app.rb', 'config.ru',
+    'app', 'bin', 'config', 'etc/flight-web-auth.yaml', 'lib'
+  ].each do |file|
+    copy file, File.expand_path("#{install_dir}/#{file}/..")
+  end
+
+  # Update the config
+  block do
+    path = File.join(install_dir, 'etc/flight-web-auth.yaml')
+    content = [
+      File.read(path),
+      "shared_secret_path: #{File.join(install_dir, 'etc/shared-secret.conf')}",
+    ].join("\n")
+    File.write path, content
+  end
+
+  # Installs the gems to the shared `vendor/share`
+  flags = [
+    '--with default',
+    "--without development test",
+    '--path vendor'
+  ].join(' ')
+  command "cd #{install_dir} && /opt/flight/bin/bundle install #{flags}", env: env
+end
