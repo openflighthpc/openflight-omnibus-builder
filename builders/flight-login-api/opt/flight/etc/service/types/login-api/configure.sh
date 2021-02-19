4e59305cad499d642d1bb8a2b9f5ed1e2e1e4e19
William McCumsite <william.mccumstie@gmail.com>
2021-02-19 13:52:53 +0000

    Allow FLIGHT_LOGIN_SSO_COOKIE_DOMAIN to be configured

---
+++ builders/flight-login-api/opt/flight/etc/service/types/login-api/configure.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#==============================================================================
+# Copyright (C) 2021-present Alces Flight Ltd.
+#
+# This file is part of OpenFlight Omnibus Builder.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on OpenFlight Omnibus Builder, please visit:
+# https://github.com/openflighthpc/openflight-omnibus-builder
+#===============================================================================
+echo "Configuring"
+
+flight_ROOT="${flight_ROOT:-/opt/flight}"
+env_file="$flight_ROOT/etc/service/env/login-api"
+
+for a in "$@"; do
+  IFS="=" read k v <<< "${a}"
+  case $k in
+    cookieDomain)
+      if cat "$env_file" | grep 'FLIGHT_LOGIN_SSO_COOKIE_DOMAIN' ; then
+        sed -i "s/FLIGHT_LOGIN_SSO_COOKIE_DOMAIN=.*/FLIGHT_LOGIN_SSO_COOKIE_DOMAIN=$v/g" "$env_file"
+      else
+        echo "FLIGHT_LOGIN_SSO_COOKIE_DOMAIN=$v" >> "$env_file"
+      fi
+      ;;
+    *)
+      echo "Unrecognised key: $k"
+    ;;
+  esac
+done
