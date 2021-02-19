9c340a732e7fa5575d4ccd34a8b5218cea3a2f80
William McCumsite <william.mccumstie@gmail.com>
2021-02-19 20:03:46 +0000

    Bump flight-job-scipt-* to version 0.6.1

--- builders/flight-job-script-webapp/config/projects/flight-job-script-webapp.rb
+++ builders/flight-job-script-webapp/config/projects/flight-job-script-webapp.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Job Script Webapp'
 
 install_dir '/opt/flight/opt/job-script-webapp'
 
-VERSION = '0.5.5'
+VERSION = '0.6.1'
 override 'flight-job-script-webapp', version: VERSION
 
 build_version VERSION
