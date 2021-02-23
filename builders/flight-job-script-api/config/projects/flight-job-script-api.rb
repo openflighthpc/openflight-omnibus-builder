80f431e13c33f7f79825182834a9fc5e60d4b1f2
William McCumsite <william.mccumstie@gmail.com>
2021-02-23 14:30:51 +0000

    Ensure the correct puma config is used

--- builders/flight-job-script-api/config/projects/flight-job-script-api.rb
+++ builders/flight-job-script-api/config/projects/flight-job-script-api.rb
@@ -35,7 +35,7 @@ VERSION = '0.6.1'
 override 'flight-job-script-api', version: VERSION
 
 build_version VERSION
-build_iteration 1
+build_iteration 2
 
 dependency 'preparation'
 dependency 'flight-job-script-api'
