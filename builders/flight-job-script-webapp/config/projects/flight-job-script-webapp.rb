a2b39f08abb83471e8de1b39916a779292cd073f
Ben Armston <ben.armston@alces-software.com>
2021-02-23 09:27:33 +0000

    Bump job-script-webapp

--- builders/flight-job-script-webapp/config/projects/flight-job-script-webapp.rb
+++ builders/flight-job-script-webapp/config/projects/flight-job-script-webapp.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Job Script Webapp'
 
 install_dir '/opt/flight/opt/job-script-webapp'
 
-VERSION = '0.5.2'
+VERSION = '0.5.5'
 override 'flight-job-script-webapp', version: VERSION
 
 build_version VERSION
