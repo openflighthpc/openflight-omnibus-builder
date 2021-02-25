c1748ef6b9bd9799440df615a1a4ac1fa276e523
William McCumsite <william.mccumstie@gmail.com>
2021-02-25 15:31:10 +0000

    Bump flight-desktop-restapi to version 2.0.0-b2

--- builders/flight-desktop-restapi/config/projects/flight-desktop-restapi.rb
+++ builders/flight-desktop-restapi/config/projects/flight-desktop-restapi.rb
@@ -31,11 +31,11 @@ friendly_name 'Flight Desktop REST API'
 
 install_dir '/opt/flight/opt/desktop-restapi'
 
-VERSION = '2.0.0-b1'
+VERSION = '2.0.0-b2'
 override 'flight-desktop-restapi', version: VERSION
 
 build_version VERSION
-build_iteration 3
+build_iteration 1
 
 dependency 'preparation'
 dependency 'update_puma_scripts'
