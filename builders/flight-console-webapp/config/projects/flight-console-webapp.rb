b3f1220633a441bb8aa34f3d4b8ea01a3d459d61
Ben Armston <ben.armston@alces-software.com>
2021-03-02 17:38:40 +0000

    Remove outdated instructions in postinst

--- builders/flight-console-webapp/config/projects/flight-console-webapp.rb
+++ builders/flight-console-webapp/config/projects/flight-console-webapp.rb
@@ -35,7 +35,7 @@ VERSION = '1.2.0-rc3'
 override 'flight-console-webapp', version: VERSION
 
 build_version VERSION
-build_iteration 1
+build_iteration 2
 
 dependency 'preparation'
 dependency 'flight-console-webapp'
