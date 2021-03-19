cce44ed9f6cacf6efc99f1572c65f676b62449c6
William McCumsite <william.mccumstie@gmail.com>
2021-03-19 14:44:38 +0000

    Bump flight-job to version 2.0.0~rc9

--- builders/flight-job/config/projects/flight-job.rb
+++ builders/flight-job/config/projects/flight-job.rb
@@ -31,11 +31,11 @@ friendly_name 'Flight Job'
 
 install_dir '/opt/flight/opt/job'
 
-VERSION = '2.0.0-rc8'
+VERSION = '2.0.0-rc9'
 override 'flight-job', version: VERSION
 
 build_version VERSION
-build_iteration 2
+build_iteration 1
 
 dependency 'preparation'
 dependency 'flight-job'
