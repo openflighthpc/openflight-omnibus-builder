0ca9f712b4ca19430f3fce8ec20bc47d0e76db73
William McCumsite <william.mccumstie@gmail.com>
2021-03-22 14:18:28 +0000

    Bump flight-job-script-api to version 0.10.4

--- builders/flight-job-script-api/config/projects/flight-job-script-api.rb
+++ builders/flight-job-script-api/config/projects/flight-job-script-api.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Job Script API'
 
 install_dir '/opt/flight/opt/job-script-api'
 
-VERSION = '0.10.1'
+VERSION = '0.10.4'
 override 'flight-job-script-api', version: VERSION
 
 build_version VERSION
