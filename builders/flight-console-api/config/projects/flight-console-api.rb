9286df5b89625ac63d96c083526d5b73afd64bb9
William McCumsite <william.mccumstie@gmail.com>
2021-04-22 18:51:43 +0100

    Bump flight-console to version 2.1.0~rc4-1

--- builders/flight-console-api/config/projects/flight-console-api.rb
+++ builders/flight-console-api/config/projects/flight-console-api.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Console api'
 
 install_dir '/opt/flight/opt/console-api'
 
-VERSION = '2.1.0-rc3'
+VERSION = '2.1.0-rc4'
 override 'flight-console-api', version: VERSION
 
 build_version VERSION
