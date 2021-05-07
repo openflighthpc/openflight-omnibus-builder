2dfd9cf9a0cb85accc935c8cd5b5f0c5c821cda7
William McCumsite <william.mccumstie@gmail.com>
2021-05-07 17:07:38 +0100

    Bump flight-file-manager-* to 1.1.0-b1

--- builders/flight-file-manager-webapp/config/projects/flight-file-manager-webapp.rb
+++ builders/flight-file-manager-webapp/config/projects/flight-file-manager-webapp.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight File Manager Webapp'
 
 install_dir '/opt/flight/opt/file-manager-webapp'
 
-VERSION = '1.0.2'
+VERSION = '1.1.0-b1'
 override 'flight-file-manager-webapp', version: VERSION
 
 build_version VERSION
