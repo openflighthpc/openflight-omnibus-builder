14bb526731a3d21df26e54bd0b1d80057d019ec2
Ben Armston <ben.armston@alces-software.com>
2021-07-05 18:42:01 +0100

    Fix issues with desktop and file manager webapp

--- builders/flight-desktop/config/projects/flight-desktop.rb
+++ builders/flight-desktop/config/projects/flight-desktop.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Desktop'
 
 install_dir '/opt/flight/opt/desktop'
 
-VERSION = '1.6.0'
+VERSION = '1.6.1'
 override 'flight-desktop', version: VERSION
 
 build_version VERSION
