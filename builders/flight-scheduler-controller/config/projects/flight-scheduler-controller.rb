4e664be003045bb3a68038e97e58f0c9a64c62dc
Ben Armston <ben.armston@alces-software.com>
2021-07-12 14:23:21 +0100

    Bump to 0.7.0

--- builders/flight-scheduler-controller/config/projects/flight-scheduler-controller.rb
+++ builders/flight-scheduler-controller/config/projects/flight-scheduler-controller.rb
@@ -31,10 +31,10 @@ friendly_name 'Flight Scheduler Controller'
 
 install_dir '/opt/flight/opt/scheduler-controller'
 
-VERSION = '0.7.0-rc1'
+VERSION = '0.7.0'
 override 'flight-scheduler-controller', version: VERSION
 build_version VERSION
-build_iteration 7
+build_iteration 1
 
 dependency 'preparation'
 dependency 'flight-scheduler-controller'
