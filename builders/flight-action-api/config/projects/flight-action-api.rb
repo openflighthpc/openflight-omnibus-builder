3be27af876347a49bd79a01f7f98873c0d0b621d
William McCumsite <william.mccumstie@gmail.com>
2021-07-14 18:49:08 +0100

    Use the standard package scripts for flight-action-api

--- builders/flight-action-api/config/projects/flight-action-api.rb
+++ builders/flight-action-api/config/projects/flight-action-api.rb
@@ -39,7 +39,9 @@ build_version VERSION
 build_iteration 3
 
 dependency 'preparation'
-dependency 'update_puma_scripts'
+# Uncomment to update the respective scripts
+# dependency 'update_puma_scripts'
+# dependency 'update_web_suite_package_scripts'
 dependency 'flight-action-api'
 dependency 'version-manifest'
 
