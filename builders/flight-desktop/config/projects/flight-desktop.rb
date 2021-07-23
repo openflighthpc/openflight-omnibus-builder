19e61ac6758f28cf66e17bdef6bf5807adda7c28
William McCumsite <william.mccumstie@gmail.com>
2021-07-23 11:58:26 +0100

    Allow alpha builds of flight-desktop

--- builders/flight-desktop/config/projects/flight-desktop.rb
+++ builders/flight-desktop/config/projects/flight-desktop.rb
@@ -32,9 +32,9 @@ friendly_name 'Flight Desktop'
 install_dir '/opt/flight/opt/desktop'
 
 VERSION = '1.6.1'
-override 'flight-desktop', version: VERSION
+override 'flight-desktop', version: ENV.fetch('ALPHA', VERSION)
 
-build_version VERSION
+build_version(ENV.key?('ALPHA') ? VERSION.sub(/(-\w+)?\Z/, '-alpha') : VERSION)
 build_iteration 2
 
 dependency 'preparation'
