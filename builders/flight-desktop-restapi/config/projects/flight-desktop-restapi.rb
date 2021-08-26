27be73599305ec8618f714f7a19dcb3b741b2b23
Ben Armston <ben.armston@alces-software.com>
2021-08-26 13:15:43 +0100

    Bump desktop-restapi

--- builders/flight-desktop-restapi/config/projects/flight-desktop-restapi.rb
+++ builders/flight-desktop-restapi/config/projects/flight-desktop-restapi.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Desktop REST API'
 
 install_dir '/opt/flight/opt/desktop-restapi'
 
-VERSION = '2.4.0-rc4'
+VERSION = '2.4.0-rc5'
 override 'flight-desktop-restapi', version: VERSION
 
 build_version VERSION
