3bfda40d239a33cbcc1f0f1c06eec06d854f82ed
Ben Armston <ben.armston@alces-software.com>
2022-08-22 13:41:47 +0100

    Bump job, desktop and desktop-restapi

--- builders/flight-job/config/projects/flight-job.rb
+++ builders/flight-job/config/projects/flight-job.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Job'
 
 install_dir '/opt/flight/opt/job'
 
-VERSION = '2.10.0'
+VERSION = '2.10.1'
 override 'flight-job', version: ENV.fetch('ALPHA', VERSION)
 
 build_version(ENV.key?('ALPHA') ? VERSION.sub(/(-\w+)?\Z/, '-alpha') : VERSION)
