0d09f57a50a7370a128d18ea3a5edb356faf56b0
VoxSecundus <millard64@hotmail.co.uk>
2023-09-14 12:20:34 +0100

    Bump Flight Profile to 1.0.0-rc1

--- builders/flight-profile/config/software/flight-profile.rb
+++ builders/flight-profile/config/software/flight-profile.rb
@@ -25,7 +25,7 @@
 # https://github.com/openflighthpc/openflight-omnibus-builder
 #===============================================================================
 name 'flight-profile'
-default_version '0.1.2'
+default_version '0.0.0'
 
 source git: 'https://github.com/openflighthpc/flight-profile'
 
