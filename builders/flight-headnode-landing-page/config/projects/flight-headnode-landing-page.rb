c9189f5572f593071e8e1c1c9e8c6318d41bcab0
Jack Millard <millard64@hotmail.co.uk>
2024-04-19 11:23:56 +0100

    Bump landing page components version

--- builders/flight-headnode-landing-page/config/projects/flight-headnode-landing-page.rb
+++ builders/flight-headnode-landing-page/config/projects/flight-headnode-landing-page.rb
@@ -32,7 +32,7 @@ friendly_name 'Headnode content for landing page'
 install_dir '/opt/flight/opt/www/landing-page/default'
 
 VERSION = '2.0.1'
-LOGIN_VERSION = '1.0.0'
+LOGIN_VERSION = '1.0.1'
 override 'flight-headnode-landing-page', version: ENV.fetch('ALPHA', VERSION)
 override 'flight-webapp-components', version: ENV.fetch('ALPHA_login', LOGIN_VERSION)
 
@@ -41,7 +41,7 @@ if ENV.key?('ALPHA') || ENV.key?('ALPHA_login')
 else
   build_version VERSION
 end
-build_iteration 1
+build_iteration 2
 
 dependency 'preparation'
 dependency 'flight-headnode-landing-page'
