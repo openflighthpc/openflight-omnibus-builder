4f0a259e37c5d5f0f8a0f683a2ed664455fd2a0c
Jack Millard <millard64@hotmail.co.uk>
2024-04-24 16:11:52 +0100

    Bump File Manager API and Webapp to 2.0.2

--- builders/flight-file-manager-api/config/projects/flight-file-manager-api.rb
+++ builders/flight-file-manager-api/config/projects/flight-file-manager-api.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight File Manager API'
 
 install_dir '/opt/flight/opt/file-manager-api'
 
-VERSION = '1.6.0'
+VERSION = '2.0.2'
 override 'flight-file-manager-api', version: ENV.fetch('ALPHA', VERSION)
 override 'flight-file-manager-backend', version: ENV.fetch('ALPHA', VERSION)
 override 'flight-file-manager-backend-proxy', version: ENV.fetch('ALPHA', VERSION)
