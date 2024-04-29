a79a451d69d93f3263dc48c3208c5d746f0ec4d8
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2024-04-29 14:22:39 +0100

    Give Slurm builder some TLC. Bump Slurm builder to flight-slurm-23.11.6-flight1 and slurm-23.11.6-flight1; PMIx to v5.0.2; NVML to 12-4-12.4.127.

--- builders/flight-slurm/build.sh
+++ builders/flight-slurm/build.sh
@@ -2,6 +2,10 @@
 REPO="https://github.com/openflighthpc/slurm"
 TARGET="$(cd "$(dirname "$0")" && pwd)/pkg"
 
+PMIX_VERSION=5.0.2
+NVML_VERSION=12-4-12.4.127
+CUDA_VERSION=12.4
+
 if [ "$1" == "--non-flight" ]; then
   nonflight=true
   shift
@@ -67,7 +71,7 @@ case $VERSION in
     fi
     ;;
   20.11)
-    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-12.3")
+    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-${CUDA_VERSION}")
     BUILD_DEPS="json-c-devel http-parser-devel jansson-devel doxygen"
     if [ -z "$nonflight" ]; then
       TAG="flight-slurm-20-11-7-1-flight1"
@@ -80,7 +84,7 @@ case $VERSION in
     nvml=true
     ;;
   21.08)
-    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-12.3")
+    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-${CUDA_VERSION}")
     BUILD_DEPS="json-c-devel http-parser-devel jansson-devel doxygen"
     if [ -z "$nonflight" ]; then
       TAG="flight-slurm-21-08-8-1-flight1"
@@ -93,7 +97,7 @@ case $VERSION in
     nvml=true
     ;;
   22.05)
-    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-12.3")
+    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-${CUDA_VERSION}")
     BUILD_DEPS="json-c-devel http-parser-devel jansson-devel doxygen"
     if [ -z "$nonflight" ]; then
       TAG="flight-slurm-22-05-10-1-flight1"
@@ -103,11 +107,10 @@ case $VERSION in
       REL="slurm-22.05.10.flight1"
     fi
     libjwt=true
-    pmix=true
     nvml=true
     ;;
   23.02)
-    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-12.3")
+    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-${CUDA_VERSION}")
     BUILD_DEPS="json-c-devel http-parser-devel jansson-devel doxygen"
     if [ -z "$nonflight" ]; then
       TAG="flight-slurm-23-02-7-1-flight1"
@@ -117,21 +120,19 @@ case $VERSION in
       REL="slurm-23.02.7.flight1"
     fi
     libjwt=true
-    pmix=true
     nvml=true
     ;;
   23.11|latest)
-    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-12.3")
+    BUILD_FLAGS=(--with slurmrestd -D "_with_nvml --with-nvml=/usr/local/cuda-${CUDA_VERSION}")
     BUILD_DEPS="json-c-devel http-parser-devel jansson-devel doxygen"
     if [ -z "$nonflight" ]; then
-      TAG="flight-slurm-23-11-1-1-flight1"
-      REL="flight-slurm-23.11.1.flight1"
+      TAG="flight-slurm-23-11-6-1-flight1"
+      REL="flight-slurm-23.11.6.flight1"
     else
-      TAG="slurm-23-11-1-1-flight1"
-      REL="slurm-23.11.1.flight1"
+      TAG="slurm-23-11-6-1-flight1"
+      REL="slurm-23.11.6.flight1"
     fi
     libjwt=true
-    pmix=true
     nvml=true
     ;;
   *)
@@ -155,7 +156,7 @@ elif grep -q 'release 9' /etc/redhat-release; then
   distro="rhel9"
   case $VERSION in
     17.11|18.08)
-      echo "$0: Slurm version not currently available for EL8: $VERSION"
+      echo "$0: Slurm version not currently available for EL9: $VERSION"
       exit 1
     ;;
   esac
@@ -184,27 +185,27 @@ if [ "$distro" == "rhel7" ]; then
       rpmbuild --rebuild ${TARGET}/../dist/libjwt-1.12.1-0.el7.src.rpm
       sudo yum install -y ~/rpmbuild/RPMS/x86_64/libjwt-devel-1.12.1-0.el7.x86_64.rpm \
            ~/rpmbuild/RPMS/x86_64/libjwt-1.12.1-0.el7.x86_64.rpm
-      build_libjwt=true
+      built_libjwt=true
     fi
   fi
-  if ! rpm -qa pmix | grep -q '^pmix-4.2.7'; then
+  if ! rpm -qa pmix | grep -q "^pmix-${PMIX_VERSION}"; then
     # Build deps:
     sudo yum install -y gcc make libevent-devel hwloc-devel python3-devel zlib-devel
-    if [ ! -f pmix-4.2.7-1.src.rpm ]; then
-      wget https://github.com/openpmix/openpmix/releases/download/v4.2.7/pmix-4.2.7-1.src.rpm
+    if [ ! -f pmix-${PMIX_VERSION}-1.src.rpm ]; then
+      wget https://github.com/openpmix/openpmix/releases/download/v${PMIX_VERSION}/pmix-${PMIX_VERSION}-1.src.rpm
     fi
-    rpmbuild --rebuild pmix-4.2.7-1.src.rpm
+    rpmbuild --rebuild pmix-${PMIX_VERSION}-1.src.rpm
     if rpm -qa pmix-devel | grep -q '^pmix-devel'; then
       sudo yum remove -y pmix-devel
     fi
     if rpm -qa pmix-tools | grep -q '^pmix-tools'; then
       sudo yum remove -y pmix-tools
     fi
-    sudo yum install -y ~/rpmbuild/RPMS/x86_64/pmix-4.2.7-1.el7.x86_64.rpm
+    sudo yum install -y ~/rpmbuild/RPMS/x86_64/pmix-${PMIX_VERSION}-1.el7.x86_64.rpm
     built_pmix=true
   fi
   if [ "$nvml" ]; then
-    sudo yum install -y https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-nvml-devel-12-3-12.3.101-1.x86_64.rpm
+    sudo yum install -y https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-nvml-devel-${NVML_VERSION}-1.x86_64.rpm
   fi
 elif [ "$distro" == "rhel8" ]; then
   sudo yum config-manager --set-enabled powertools
@@ -214,31 +215,21 @@ elif [ "$distro" == "rhel8" ]; then
        numactl-devel hdf5-devel lz4-devel freeipmi-devel \
        rrdtool-devel gtk2-devel libcurl-devel mariadb-devel \
        man2html python3 python2 $BUILD_DEPS
-  if ! rpm -qa pmix | grep -q '^pmix-4.2.7'; then
+  if ! rpm -qa pmix | grep -q "^pmix-${PMIX_VERSION}"; then
     # Build deps:
     sudo yum install -y gcc make libevent-devel hwloc-devel python3-devel zlib-devel
-    if [ ! -f pmix-4.2.7-1.src.rpm ]; then
-      wget https://github.com/openpmix/openpmix/releases/download/v4.2.7/pmix-4.2.7-1.src.rpm
+    if [ ! -f pmix-${PMIX_VERSION}-1.src.rpm ]; then
+      wget https://github.com/openpmix/openpmix/releases/download/v${PMIX_VERSION}/pmix-${PMIX_VERSION}-1.src.rpm
     fi
-    rpmbuild --rebuild pmix-4.2.7-1.src.rpm
+    rpmbuild --rebuild pmix-${PMIX_VERSION}-1.src.rpm
     if rpm -qa pmix-devel | grep -q '^pmix-devel'; then
       sudo yum remove -y pmix-devel
     fi
     if rpm -qa pmix-tools | grep -q '^pmix-tools'; then
       sudo yum remove -y pmix-tools
     fi
-    sudo yum install -y ~/rpmbuild/RPMS/x86_64/pmix-4.2.7-1.el8.x86_64.rpm
+    sudo yum install -y ~/rpmbuild/RPMS/x86_64/pmix-${PMIX_VERSION}-1.el8.x86_64.rpm
     built_pmix=true
-  fi
-  if [ "$libssh2" ]; then
-    # This is needed for Slurm 18.08 or 17.11.
-    if ! rpm -qa libssh2-devel | grep -q '^libssh2-devel'; then
-      if [ ! -f libssh2-1.8.0-3.el7.src.rpm ]; then
-        wget http://vault.centos.org/7.7.1908/os/Source/SPackages/libssh2-1.8.0-3.el7.src.rpm
-      fi
-      rpmbuild --rebuild libssh2-1.8.0-3.el7.src.rpm
-      sudo yum install -y ~/rpmbuild/RPMS/x86_64/libssh2-1.8.0-3.el8.x86_64.rpm ~/rpmbuild/RPMS/x86_64/libssh2-devel-1.8.0-3.el8.x86_64.rpm
-    fi
   fi
 
   if [ "$libjwt" ]; then
@@ -247,7 +238,7 @@ elif [ "$distro" == "rhel8" ]; then
   fi
 
   if [ "$nvml" ]; then
-    sudo yum install -y https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-nvml-devel-12-3-12.3.101-1.x86_64.rpm
+    sudo yum install -y https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-nvml-devel-${NVML_VERSION}-1.x86_64.rpm
   fi
 elif [ "$distro" == "rhel9" ]; then
   sudo yum config-manager --set-enabled crb
@@ -257,44 +248,30 @@ elif [ "$distro" == "rhel9" ]; then
        rrdtool-devel gtk2-devel libcurl-devel mariadb-devel \
        man2html python3 dbus-devel $BUILD_DEPS
   sudo yum install -y pmix-devel
-  if ! rpm -qa pmix | grep -q '^pmix-4.2.7'; then
+  if ! rpm -qa pmix | grep -q "^pmix-${PMIX_VERSION}"; then
     # Build deps:
     sudo yum install -y gcc make libevent-devel hwloc-devel python3-devel zlib-devel
-    if [ ! -f pmix-4.2.7-1.src.rpm ]; then
-      wget https://github.com/openpmix/openpmix/releases/download/v4.2.7/pmix-4.2.7-1.src.rpm
+    if [ ! -f pmix-${PMIX_VERSION}-1.src.rpm ]; then
+      wget https://github.com/openpmix/openpmix/releases/download/v${PMIX_VERSION}/pmix-${PMIX_VERSION}-1.src.rpm
     fi
-    rpmbuild --rebuild pmix-4.2.7-1.src.rpm
+    rpmbuild --rebuild pmix-${PMIX_VERSION}-1.src.rpm
     if rpm -qa pmix-devel | grep -q '^pmix-devel'; then
       sudo yum remove -y pmix-devel
     fi
     if rpm -qa pmix-tools | grep -q '^pmix-tools'; then
       sudo yum remove -y pmix-tools
     fi
-    sudo yum install -y ~/rpmbuild/RPMS/x86_64/pmix-4.2.7-1.el9.x86_64.rpm
+    sudo yum install -y ~/rpmbuild/RPMS/x86_64/pmix-${PMIX_VERSION}-1.el9.x86_64.rpm
     built_pmix=true
   fi
 
- # if [ "$libssh2" ]; then
- #   # This is needed for Slurm 18.08 or 17.11.
- #   if ! rpm -qa libssh2-devel | grep -q '^libssh2-devel'; then
- #     if [ ! -f libssh2-1.8.0-3.el7.src.rpm ]; then
- #       wget http://vault.centos.org/7.7.1908/os/Source/SPackages/libssh2-1.8.0-3.el7.src.rpm
- #     fi
- #     rpmbuild --rebuild libssh2-1.8.0-3.el7.src.rpm
- #     sudo yum install -y ~/rpmbuild/RPMS/x86_64/libssh2-1.8.0-3.el8.x86_64.rpm ~/rpmbuild/RPMS/x86_64/libssh2-devel-1.8.0-3.el8.x86_64.rpm
- #   fi
- # fi
-
- # if [ "$libjwt" ]; then
- #   # This is needed for Slurm 20.11.
- #   sudo yum install -y check-devel
- #   rpmbuild --rebuild ${TARGET}/../dist/libjwt-1.12.1-0.el7.src.rpm
- #   sudo yum install -y ~/rpmbuild/RPMS/x86_64/libjwt-devel-1.12.1-0.el8.x86_64.rpm \
- #        ~/rpmbuild/RPMS/x86_64/libjwt-1.12.1-0.el8.x86_64.rpm
- # fi
+  if [ "$libjwt" ]; then
+    # This is needed for Slurm 20.11+
+    sudo yum install -y libjwt-devel
+  fi
 
   if [ "$nvml" ]; then
-    sudo yum install -y https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-nvml-devel-12-3-12.3.101-1.x86_64.rpm
+    sudo yum install -y https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-nvml-devel-${NVML_VERSION}-1.x86_64.rpm
   fi
 fi
 
@@ -350,19 +327,10 @@ else
   mv $HOME/rpmbuild/RPMS/*/slurm-*.rpm "$TARGET"
 fi
 
-if [ "$libssh2" ]; then
-  # This is needed for Slurm 18.08 or 17.11.
-  if [ "$distro" == "rhel8" ]; then
-    mv $HOME/rpmbuild/RPMS/*/libssh2-1.8.0-3.el8.* "$TARGET"
-  fi
-fi
-
-if [ "$build_libjwt" ]; then
-  # This is needed for Slurm 20.11.
+if [ "$built_libjwt" ]; then
+  # This is needed for Slurm 20.11+.
   if [ "$distro" == "rhel7" ]; then
-    mv ~/rpmbuild/RPMS/x86_64/libjwt-1.12.1-0.el7.x86_64.rpm "$TARGET"
-  elif [ "$distro" == "rhel8" ]; then
-    mv ~/rpmbuild/RPMS/x86_64/libjwt-1.12.1-0.el8.x86_64.rpm "$TARGET"
+    mv ~/rpmbuild/RPMS/x86_64/libjwt-*.rpm "$TARGET"
   fi
 fi
 
