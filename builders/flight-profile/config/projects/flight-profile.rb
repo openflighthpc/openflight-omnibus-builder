d5a00b352014f22256c7b12db751882fadaf140f
Jack Millard <millard64@hotmail.co.uk>
2024-06-10 11:16:09 +0100

    Bump Profile to 1.1.1

--- builders/flight-profile/config/projects/flight-profile.rb
+++ builders/flight-profile/config/projects/flight-profile.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Profile'
 
 install_dir '/opt/flight/opt/profile'
 
-VERSION = '1.1.0'
+VERSION = '1.1.1'
 override 'flight-profile', version: VERSION
 
 build_version VERSION
